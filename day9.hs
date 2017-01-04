626dc98dbd9957f4264609866f75143ae2d15fcd
Bob Wall <bob.wall@ironcorelabs.com>
2017-01-04 09:03:05 -0700

    Added day 9 solution

---
+++ day9.hs
@@ -0,0 +1,11 @@
+--  Haskell code to solve the problem presented in Advent of Code 2016, day 9.
+--  Author: Bob Wall
+module AOCDay9 where
+
+import Data.List
+
+
+
+printDay9Solutions :: IO ()
+printDay9Solutions = do
+  content <- readFile "day9.in"
